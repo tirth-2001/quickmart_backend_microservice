ea97fb4cbdd4ba22e87e7d8397dd64af6173f28b
Tirth <tirthgpatel.27@gmail.com>
2023-12-10 10:11:32 +0530

    [shared-1.0.6] swagger config change

--- shared-service/src/swagger/swaggerConfig.ts
+++ shared-service/src/swagger/swaggerConfig.ts
@@ -1,12 +1,14 @@
 export const swaggerConfig = {
-  openapi: '3.0.0',
-  info: {
-    version: '1.0.0',
-    title: 'Quickmart Microservices API',
-    description: 'API documentation for Quickmart Microservices',
-    contact: {
-      name: 'Tirth Patel',
-      email: 'tirthgpatel.27@gmail.com',
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      version: '1.0.0',
+      title: 'Quickmart Microservices API',
+      description: 'API documentation for Quickmart Microservices',
+      contact: {
+        name: 'Tirth Patel',
+        email: 'tirthgpatel.27@gmail.com',
+      },
     },
   },
 }
